bbb7fe84a247d8e631028a42feba6b380e9ede17
webhoster2024 <84992437+adhil12345@users.noreply.github.com>
2024-01-16 01:10:09 +0530

    Create afflinkgen.js

---
+++ js/afflinkgen.js
@@ -0,0 +1,55 @@
+ document.addEventListener("DOMContentLoaded", function() {
+        var getAffiliatDe = document.querySelector("#AliExpressDetaiels").innerHTML;
+
+        const proNam = getAffiliatDe.split("ProductName[")[1].split("]")[0];
+        const afflink = getAffiliatDe.split("ProductAffiliateLnk[")[1].split("]")[0];
+        const afflprorating = parseFloat(getAffiliatDe.split("RatingCount[")[1].split("]")[0]);
+
+        // Create a new div element
+        const newDiv = document.createElement("div");
+        newDiv.className = "affi-price-main-display";
+
+        // Set the HTML content
+        const displayAff = newDiv.innerHTML = `
+            <div class="affi-pro-img-display">${afflprorating}</div>
+            <div class="affi-pro-name-display">
+                <div class="affi-pro-name">${proNam}</div>
+                <div class="affi-pro-ratring">${generateStars(afflprorating)}<span> ${afflprorating}</span></div>
+            </div>
+            <div class="affi-pro-aliExpress-button">
+                <a target="_blank" href="${afflink}">Check Price on <span style="color:red;">AliExpress</span></a>
+            </div>
+        `;
+        
+        const checkAf = afflink.split(":")[0];
+        
+        if(afflink === checkAf){
+        	document.querySelector("#displayAffLinkz").style.display = "none";
+        } else {
+			document.querySelector("#displayAffLinkz").innerHTML = displayAff;
+		}
+
+        
+    });
+
+    function generateStars(rating) {
+        const fullStars = Math.floor(rating);
+        const halfStar = rating % 1 !== 0;
+        const emptyStars = 5 - Math.ceil(rating);
+
+        let stars = '';
+
+        for (let i = 0; i < fullStars; i++) {
+            stars += '⭐';
+        }
+
+        if (halfStar) {
+            stars += '⭐';
+        }
+
+        for (let i = 0; i < emptyStars; i++) {
+            stars += '☆';
+        }
+
+        return stars;
+    }
